d553b0d48d78a907dd9373186415a78a9e005968
Alexandru Voinescu <marius.a.voinescu@gmail.com>
2019-02-27 15:58:31 +0200

    test

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 signInWidgetConfig = {
     // Enable or disable widget functionality with the following options. Some of these features require additional configuration in your Okta admin settings. Detailed information can be found here: https://github.com/okta/okta-signin-widget#okta-sign-in-widget
     // Look and feel changes:
-    logo: '//logo.clearbit.com/okta.com', // Try changing "okta.com" to other domains, like: "workday.com", "splunk.com", or "delmonte.com"
+    //logo: 'https://ok2static.oktacdn.com/bc/image/fileStoreRecord?id=fs08uajy6jJndJBHQ0x7', // Try changing "okta.com" to other domains, like: "workday.com", "splunk.com", or "delmonte.com"
     language: 'en',                       // Try: [fr, de, es, ja, zh-CN] Full list: https://github.com/okta/okta-signin-widget#language-and-text
     i18n: {
       //Overrides default text when using English. Override other languages by adding additional sections.
@@ -22,39 +22,29 @@ signInWidgetConfig = {
       //callRecovery: true,               // Enable voice call-based account recovery
       router: true,                       // Leave this set to true for the API demo
     },
-    baseUrl: 'https://live-widget.oktapreview.com',
+    baseUrl: 'https://inam.oktapreview.com',
     clientId: '0oaexo9c530ZUVuOj0h7',
-    redirectUri: 'https://developer.okta.com/live-widget',
+    redirectUri: 'https://inam.oktapreview.com/',
     authParams: {
-      issuer: 'https://live-widget.oktapreview.com/oauth2/ausexqn31sz3HMxdf0h7',
-      responseType: ['id_token', 'token'],
-      scopes: ['openid', 'email', 'profile'],
+    issuer: 'https://inam.oktapreview.com/oauth2/default',
+    responseType: ['id_token', 'token'],
+    scopes: ['openid', 'email', 'profile'],
     },
   };
-  
-  signInWidget = new OktaSignIn(signInWidgetConfig);
-  
-  function widgetSuccessCallback(res) {
-    var key = '';
-    if (res[0]) {
-      key = Object.keys(res[0])[0];
-      signInWidget.tokenManager.add(key, res[0]);
-    }
-    if (res[1]) {
-      key = Object.keys(res[1])[0];
-      signInWidget.tokenManager.add(key, res[1]);
-    }
+
+  var signIn = new OktaSignIn(signInWidgetConfig);
+
+  signIn.renderEl({
+    el: '#widget-container'
+  }, function success(res) {
     if (res.status === 'SUCCESS') {
-      var token = signInWidget.tokenManager.get(key);
-      console.log("Logged in to Okta and issued token:");
-      console.log(token);
-      console.log("Reload this page to start over.");
-      alert("Logged in! Check your developer console for details");
+      res.session.setCookieAndRedirect('https://tosatsu.okta.com/app/UserHome');
+    } else {
+      // The user can be in another authentication state that requires further action.
+      // For more information about these states, see:
+      //   https://github.com/okta/okta-signin-widget#rendereloptions-success-error
     }
-  }
-  
-  function widgetErrorCallback (err) {
-  }
-  
-  signInWidget.renderEl({el: '#widget-container'}, widgetSuccessCallback, widgetErrorCallback);
+  });
           
+  baseUrl: 'https://inam.oktapreview.com',
+  
